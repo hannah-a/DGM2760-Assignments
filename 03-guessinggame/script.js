69169e669ef4c95191573cdee8e21cabfd63bad2
Hannah Armstrong <hannahlinarmstrong@gmail.com>
2022-02-02 18:27:39 -0700

    finished game

--- 03-guessinggame/script.js
+++ 03-guessinggame/script.js
@@ -1,24 +1,27 @@
 
-let correctNumber = Math.floor(Math.random() * 15)
+let correctNumber = Math.floor(Math.random() * 15) //Picks number from 0 to 14 
+correctNumber += 1 //so that our given numbers are now 1 through 15
 
 
+let totalGuesses = 0 //setting initial variable to zero
+let gamerGuess = 0 //setting intial variable to zero
 
-let guessed = false
-let totalGuesses = 0 
-let gamerGuess = 0 
-correctNumber += 1
 
 console.log(` the correct number is ${correctNumber}`)
 
 function attemptInput() {
     totalGuesses += 1 // increments totalGuesses +1 for every guess
     gamerGuess = document.querySelector('#guess').value
-    console.log(totalGuesses, gamerGuess)
     const feedback = document.querySelector('#feedback') // handle for the too low or too high variable
 
     if (gamerGuess == correctNumber) {
-        feedback.innerText = 'Correct!'
+        feedback.innerText = 'You Win an Award!'
         giveAward()
+        //After displaying ribbon the button will turn into a reset button and force reload the document
+        let button = document.querySelector('.guessButton')
+        button.innerText= 'Reset'
+        button.setAttribute('onclick', "window.location.reload()")
+    //Actions that happen if they do not guess the right answer:
     } else if(gamerGuess > correctNumber && gamerGuess < 16) {
         feedback.innerText = 'Too High, Try Again'
     } else if(gamerGuess < correctNumber && gamerGuess > 0) {
@@ -27,11 +30,10 @@ function attemptInput() {
         feedback.innerText = 'Please choose a number between 1 and 15 and try again'
         totalGuesses -= 1
     }
-    document.querySelector(".attemptNumber").innerText = totalGuesses
+    document.querySelector(".attemptNumber").innerText = `Attempts: ${totalGuesses}`
 }
 
-function giveAward() {
-    console.log('congrats')
+function giveAward() { //Displays correct ribbon based on number of attempts
     let imagePath = "#"
     switch(totalGuesses) {
         case 1:
