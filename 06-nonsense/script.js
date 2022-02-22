4b4b27a215e0a39a4a4a54972b14b4d5cf6170cd
hannah-a <hannahlinarmstrong@gmail.com>
2022-02-21 21:58:06 -0700

    class video content/ start file

---
+++ 06-nonsense/script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('#createButton')
+
+function tellStory() {
+//const nounArray = querySelector('.nouns')
+
+const myStory = `Once upon a time${nounArray[0]}`
+
+//get a reference to the querySelector('#story')
+}
