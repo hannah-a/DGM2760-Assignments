dcb5defe6310fb87b69bcf7ee40411775ab1d561
hannah-a <hannahlinarmstrong@gmail.com>
2022-02-26 10:19:48 -0700

    finished javascript regex

--- 06-nonsense/script.js
+++ 06-nonsense/script.js
@@ -4,21 +4,19 @@ const button = document.querySelector('#createButton').addEventListener('click',
 })
 
 function tellStory() {
-//const nounArray = querySelector('.nouns')
+
 const nounsString = document.querySelector('#nouns').value 
 const verbsString = document.querySelector('#verbs').value
 const adjString = document.querySelector('#adjectives').value 
-console.log(nounsString, verbsString, adjString)
 
-//replace space with enters
-//split string on enters
+const nounArray = nounsString.toLowerCase().trim().split(/\s+/, 3)
+const verbArray = verbsString.toLowerCase().trim().split(/\s+/, 3)
+const adjArray  = adjString.toLowerCase().trim().split(/\s+/, 3)
+console.log(nounArray, verbArray, adjArray)
 
-// const myStory = `Once upon a time in ${noun[0]}, the Minister of Health decreed that every ${noun[1]} would be confiscated. This is due to the fact that ${adjective[0]} ${noun[2]} ${verb[0]} in the park fountains. No one liked this new decree so they decided to ${verb[1]} the ${adjective[1]} Minister of Health. Watching him ${verb[2]} in the ${adjective[2]} was the funniest thing to ever happen. The End`
-console.log('working button')
-//get a reference to the querySelector('#story')
+const myStory = `Once upon a time in ${nounArray[0]}, the Minister of Health decreed that every ${adjArray[2]} ${nounArray[1]} would be confiscated. This is due to the fact that ${adjArray[0]} ${nounArray[2]}s ${verbArray[0]} in the park fountains. No one liked this new decree so they decided to ${verbArray[1]} the ${adjArray[1]} Minister of Health. Watching him ${verbArray[2]} in the fountain was the funniest thing to ever happen. The End.`
+const storyDiv = document.querySelector('.story').textContent = myStory
+
 }
 
-//noun0 to uppercase
-//noun2 needs plural
-//splice the arrays so theres no more than 3 per array
-//create error message when someone inputs more than three words, or inputs other characters
+
