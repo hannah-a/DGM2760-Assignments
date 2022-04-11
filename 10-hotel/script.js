3d486ce189a1a0fb394bf54b7caeb9a1d03aeaf6
hannah-a <hannahlinarmstrong@gmail.com>
2022-04-10 22:09:27 -0600

    finished js and css

--- 10-hotel/script.js
+++ 10-hotel/script.js
@@ -9,25 +9,45 @@ async function getHotelData() {
 
 }
 
-let hotelData = {} //trying to get an empty object
+let hotelData = {} //filled with data 
+
 getHotelData().then(data => {
   return hotelData = data, //returns an empty object but takes longer because its asynchronous
   console.log(hotelData)
 })
 
-const buttonsAll = document.querySelectorAll('.buttonDiv__button').addEventListener('click', hotelInfo)
+const buttonsAll = document.querySelectorAll('.buttonDiv__button')
+buttonsAll.forEach(button => {
+  button.addEventListener('click', hotelInfo)
+})
 
-for (button in buttonsAll) {
- 
-}
+console.log(buttonsAll)
+
 document.querySelector('#button__estate').addEventListener('click', hotelInfo) //no parenthesis because you're not calling the function, your passing it as a parameter
 //functions are first class objects that can be passed around
 
 function hotelInfo(event) {
-  let hotelChoice = hotelData.hotels.find(hotel => {
-    return event.target.id === hotel.name
-  })
-  //find a match 
+ let hotelChoice = hotelData.hotels.find(hotel => {
+ return event.target.id === hotel.id
+ })
 
-  //query selectors to get text content and template literals, 
+ //nameImageAddressDiv
+ document.querySelector('.main__name').textContent = hotelChoice.name
+
+//  document.querySelector('.main__figure').innerHTML = `<img src="/10-hotel/images/${hotelChoice.picture}" alt="${hotelChoice.name}" class="main__figureImage">` 
+ document.querySelector('.main__nameImageAddressDiv').style.backgroundImage = `url('/10-hotel/images/${hotelChoice.picture}')`
+ document.querySelector('.main__nameImageAddressDiv').style.backgroundRepeat = 'no-repeat'
+ document.querySelector('.main__nameImageAddressDiv').style.backgroundSize = 'cover'
+ document.querySelector('.main__nameImageAddressDiv').style.backgroundPosition = 'center'
+
+
+ document.querySelector('.main__addressDiv').textContent = hotelChoice.address
+
+ //amnListBookDiv
+ document.querySelector('.main__amnListDiv').innerHTML = `Rooms: ${hotelChoice.rooms} <br>
+ Room Types: ${hotelChoice.roomtypes} <br>
+ Gym: ${hotelChoice.gym} <br>
+ Pool: ${hotelChoice.pool} <br>
+ Breakfast: ${hotelChoice.breakfast} <br>
+ `
 }
